094e904da094456061f32424c6b2c81dc1ac21d6
neryad <dayern.gomez@hotmail.com>
2021-03-06 23:47:38 -0400

    index routes

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ app.use(express.json());
 //** Routes
 app.use('/api/auth', require('./routes/auth'));
 
+//* others routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public/index.html'));
+// });
+
 app.listen(process.env.PORT, () => {
   console.log(`Server online on port ${process.env.PORT}`);
 });
